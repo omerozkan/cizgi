840d3ca9fdd8b51d7bf89f216b962a527c2c55f5
Ömer Özkan <omer@ozkan.info>
2013-03-24 17:43:14 +0200

    View sınıfı oluşturuldu

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -6,13 +6,15 @@
 		public function indexAction()
 		{
 			$this->view->message = "Bu bir deneme mesajıdır. Controller tarafından üretilmiştir.";
+			if(isset($_SERVER['PATH_INFO']))
+			{
+				echo $_SERVER['PATH_INFO'];
+			}
 		}
 		
 		public function editAction()
 		{
 		
 			$this->view->message = "Bu bir deneme mesajıdır. edit action tarafından üretilmiştir.";
-			
-			
 		}
 	}
