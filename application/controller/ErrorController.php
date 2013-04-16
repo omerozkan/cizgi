7951eeae50c65cfcde0318040e94186b3331a37a
Ömer Özkan <omer@ozkan.info>
2013-04-16 15:53:20 +0300

    Error ve Index Controller sınıfları eklendi

---
+++ application/controller/ErrorController.php
@@ -0,0 +1,12 @@
+<?php
+class Controller_Error extends Cizgi_Controller
+{
+	function init() {
+	}
+
+	function indexAction() {
+		echo "Aradığınız sayfaya ulaşılamadı!";
+	}
+
+	
+}
---
+++ application/controller/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+class Controller_Index extends Cizgi_Controller
+{
+	function init() {
+	}
+
+	function indexAction() {
+		echo "Merhaba Çizgi!";
+	}
+}
