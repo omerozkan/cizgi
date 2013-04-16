0c909e231736c6d8be72162d90eb806a4af88973
Ömer Özkan <omer@ozkan.info>
2013-04-16 15:20:54 +0300

    Uygulama için autoload fonksiyonu yazıldı

---
+++ library/cizgi/autoload.php
@@ -0,0 +1,27 @@
+<?php
+require ROOT_PATH."/library/cizgi/Autoloader.php";
+spl_autoload_register("__autoload");
+$AUTOLOADER = null;
+
+function __autoload($name)
+{
+	global $AUTOLOADER;
+	initializeAutoLoader ( $AUTOLOADER );
+	$file = $AUTOLOADER->getClassFile($name);
+	if(file_exists($file))
+		require $file;
+	else
+	{
+	//	throw new Cizgi_Class_Not_Found_Exception("class: $name\n<br> file: $file has not found!");
+	}
+}
+
+function initializeAutoLoader() {
+	global $AUTOLOADER;
+	if(is_null($AUTOLOADER))
+	{
+		$AUTOLOADER = new Cizgi_Autoloader();
+	}
+}
+
+class Cizgi_Class_Not_Found_Exception extends RuntimeException{}
