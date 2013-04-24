0088c29f2026d28e726b179382f4b3dd243a6e55
Ömer Özkan <omer@ozkan.info>
2013-04-24 19:54:07 +0300

    smarty için denemeler yapıldı

--- application/controller/IndexController.php
+++ application/controller/IndexController.php
@@ -5,11 +5,16 @@ class Controller_Index extends Cizgi_Controller
 	}
 
 	function indexAction() {
-		$this->view->assign("name", "Cizgi");
+	//	$this->view->assign("name", "Cizgi");
+		$this->view->name = "Cizgi";
 	}
 	
 	function secondAction()
 	{
 		$this->view->assign("message", "Second action is working now");
+		
+		$example = new Model_Example();
+		$example->name = 'Ömer ÖZKAN';
+		$this->view->assign('object', $example);
 	}
 }
