8376ae0ab1f9daf9b28161b599c686224be47bc1
Ömer Özkan <omer@ozkan.info>
2013-04-25 02:35:15 +0300

    Çizgi layout işlemleri için plugin eklendi

---
+++ library/smarty/plugins/function.cizgiview.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Smarty plugin
+* -------------------------------------------------------------
+* File:     function.cizgiview.php
+* Type:     function
+* Name:     load
+* Purpose:  to load cizgi view file
+* -------------------------------------------------------------
+*/
+function smarty_function_cizgiview($params, &$smarty)
+{
+	$smarty->renderView();
+}
