ab68ebcdc643f376a9c3aca776135e78aa77ac52
Ömer Özkan <omer@ozkan.info>
2013-04-24 21:34:08 +0300

    tested layouts

--- application/Configuration.php
+++ application/Configuration.php
@@ -1,17 +1,19 @@
 <?php
 
 class Configuration {
-    static $hostname = "localhost";
-	static $dbUser = "root";
-	static $dbPassword = "";
-	static $db = "none";
 	static $defaultController = "index";
 	static $errorController = "error";
-	static $defaultLayout = 'index';
 	static $url = "http://localhost/cizgi/";
 	static $publicDirectory = "public";
 	static $defaultExtention = "html";
 	static $sessionKey = "CIZGI_CHANGEME";
+	static $viewLayout = 1;
+	static $viewDefaultLayout = 'index';
+	static $viewDefaultExtention = "phtml";
+    static $dbHostname = "localhost";
+	static $dbUser = "root";
+	static $dbPassword = "";
+	static $dbName = "none";
 }
 
 ?>
