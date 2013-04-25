feaca37dc92de7d0ceee83a795148e9e5dd3239b
Ömer Özkan <omer@ozkan.info>
2013-04-25 21:16:39 +0300

    html

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -6,6 +6,7 @@ class Bootstrap extends Cizgi_Bootstrap
 		$urlDispatcher = new Cizgi_URLDispatcher();
 		$urlDispatcher->setDefaultController(Configuration::$defaultController);
 		$view = new Cizgi_View();
+		$view->setHTMLData('title', 'Çizgi Uygulama Çatısı');
 		parent::__construct($urlDispatcher, $view);
 		$this->setDeveloperMode(true);
 	}
